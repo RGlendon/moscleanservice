9582444acb513343569ce759e625a9de4e2504e1
RGlendon <Rold.Glendon@gmail.com>
2020-04-06 17:06:32 +0300

    Updates

---
+++ precache-manifest.3f54a8be5032c944baadf66e58b8491f.js
@@ -0,0 +1,290 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8091a81d526c19e52931722acbdaee1f",
+    "url": "/moscleanservice/index.html"
+  },
+  {
+    "revision": "53b062359f0c368f0d83",
+    "url": "/moscleanservice/static/css/main.e5537f19.chunk.css"
+  },
+  {
+    "revision": "15a952d548932a027a32",
+    "url": "/moscleanservice/static/js/2.064fc7b7.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/moscleanservice/static/js/2.064fc7b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53b062359f0c368f0d83",
+    "url": "/moscleanservice/static/js/main.6a5aa361.chunk.js"
+  },
+  {
+    "revision": "01b7c41f41976ce187ca",
+    "url": "/moscleanservice/static/js/runtime-main.100cb92c.js"
+  },
+  {
+    "revision": "50d1f3b09b3a2865fc6bf7c1ac033cc4",
+    "url": "/moscleanservice/static/media/5th1.50d1f3b0.jpg"
+  },
+  {
+    "revision": "4b84e4358af3bef588a7f9fdef61e6e8",
+    "url": "/moscleanservice/static/media/5th2.4b84e435.jpg"
+  },
+  {
+    "revision": "35a06ad80b9bb22e529495c048f39c1b",
+    "url": "/moscleanservice/static/media/5th3.35a06ad8.jpg"
+  },
+  {
+    "revision": "915d107e1673c9841fac27d3b6d9a990",
+    "url": "/moscleanservice/static/media/Bucket.915d107e.svg"
+  },
+  {
+    "revision": "c6ed0ee41744901cd43f52908c43a4d7",
+    "url": "/moscleanservice/static/media/Chair.c6ed0ee4.svg"
+  },
+  {
+    "revision": "73905c759fc365d833f33751f62a2051",
+    "url": "/moscleanservice/static/media/Cleaner.73905c75.svg"
+  },
+  {
+    "revision": "fceec93838d459feceaa645a2a09d14a",
+    "url": "/moscleanservice/static/media/Cleaner.fceec938.svg"
+  },
+  {
+    "revision": "e2c6fd91f4dcb8b445fad3847d396847",
+    "url": "/moscleanservice/static/media/Clock.e2c6fd91.svg"
+  },
+  {
+    "revision": "23e51f2da8ee219f4efea9f65c811917",
+    "url": "/moscleanservice/static/media/Hand.23e51f2d.svg"
+  },
+  {
+    "revision": "ca12a719cd344dd391d9f51e7bbd9b33",
+    "url": "/moscleanservice/static/media/HelveticaNeueCyr-Bold.ca12a719.woff"
+  },
+  {
+    "revision": "e914a2dd1cd1d3c3944e0ad0e950046d",
+    "url": "/moscleanservice/static/media/HelveticaNeueCyr-Light.e914a2dd.woff"
+  },
+  {
+    "revision": "2a615283a212836fd6c33bf32bc87ebf",
+    "url": "/moscleanservice/static/media/HelveticaNeueCyr-Medium.2a615283.woff"
+  },
+  {
+    "revision": "510e1c045e61e20233f9ecf23d031703",
+    "url": "/moscleanservice/static/media/HelveticaNeueCyr-Roman.510e1c04.woff"
+  },
+  {
+    "revision": "2d30fa8ddc7245d31ca4123e7d2ced5e",
+    "url": "/moscleanservice/static/media/Hoover.2d30fa8d.svg"
+  },
+  {
+    "revision": "207529fa02e14bbb82aafdfebde96f10",
+    "url": "/moscleanservice/static/media/Image1.207529fa.jpg"
+  },
+  {
+    "revision": "40769490b744898d5bcd2422d7903321",
+    "url": "/moscleanservice/static/media/Image2.40769490.jpg"
+  },
+  {
+    "revision": "040d0d932d862eacca2930ab6a524b82",
+    "url": "/moscleanservice/static/media/Inst.040d0d93.svg"
+  },
+  {
+    "revision": "fab1b6fc3b88536904d37d73d3a191e0",
+    "url": "/moscleanservice/static/media/Logo.fab1b6fc.svg"
+  },
+  {
+    "revision": "3bd880796a55e083c746dab9e8e978f1",
+    "url": "/moscleanservice/static/media/LogoClear2.3bd88079.svg"
+  },
+  {
+    "revision": "8093062433ea8244254bb83482c0a764",
+    "url": "/moscleanservice/static/media/MENUburg.80930624.svg"
+  },
+  {
+    "revision": "0da393c84e8e845c777fd30fc4022cae",
+    "url": "/moscleanservice/static/media/Medal.0da393c8.svg"
+  },
+  {
+    "revision": "7db96b16c79d3b50e978e79fedd1df6a",
+    "url": "/moscleanservice/static/media/Mobile.7db96b16.svg"
+  },
+  {
+    "revision": "58308bcc8dce00a629bd46305a1c5494",
+    "url": "/moscleanservice/static/media/OpenSans-Bold.58308bcc.woff"
+  },
+  {
+    "revision": "600da97b1dd5094a4175dd2fd491ca25",
+    "url": "/moscleanservice/static/media/Rectangle235.600da97b.jpg"
+  },
+  {
+    "revision": "bb02cb44900c2c11ee0a78aa52a07388",
+    "url": "/moscleanservice/static/media/Shield.bb02cb44.svg"
+  },
+  {
+    "revision": "7674b59b1b4e905c8ea0b5a4d4f1a837",
+    "url": "/moscleanservice/static/media/Staff.7674b59b.svg"
+  },
+  {
+    "revision": "9279040b0f80307f666408e2cdadff69",
+    "url": "/moscleanservice/static/media/Station.9279040b.svg"
+  },
+  {
+    "revision": "4f25bd72593002f0b0235a0f21375dfd",
+    "url": "/moscleanservice/static/media/VK.4f25bd72.svg"
+  },
+  {
+    "revision": "f6aecb9187f40153f2a58d3838be9e67",
+    "url": "/moscleanservice/static/media/arrowDown.f6aecb91.svg"
+  },
+  {
+    "revision": "4e8a844e5b47aa6e7463f614ec65c247",
+    "url": "/moscleanservice/static/media/arrowDownSelect.4e8a844e.svg"
+  },
+  {
+    "revision": "67df91e79bb48dc43c41ea69949273ef",
+    "url": "/moscleanservice/static/media/arrowLeftSwipe.67df91e7.svg"
+  },
+  {
+    "revision": "2d94537546ef9b90f02e9f199eaa94bb",
+    "url": "/moscleanservice/static/media/arrowRightSwipe.2d945375.svg"
+  },
+  {
+    "revision": "9428c3a35d164251d1fb96b2d01d9a73",
+    "url": "/moscleanservice/static/media/bottles.9428c3a3.svg"
+  },
+  {
+    "revision": "5ff73b5a16fbd97933450cab36e1581b",
+    "url": "/moscleanservice/static/media/carpet.5ff73b5a.svg"
+  },
+  {
+    "revision": "5388a838eed75345825612a8ce79cc77",
+    "url": "/moscleanservice/static/media/carpet2.5388a838.svg"
+  },
+  {
+    "revision": "bc893abf4ad08724da2e5846fbc271fb",
+    "url": "/moscleanservice/static/media/chair(2).bc893abf.svg"
+  },
+  {
+    "revision": "3fdaddc78ae4248a6a8314e31b867e07",
+    "url": "/moscleanservice/static/media/chair-sofa.3fdaddc7.svg"
+  },
+  {
+    "revision": "f62055c463abbf6c8a52ec722daf80b5",
+    "url": "/moscleanservice/static/media/chairComp.f62055c4.svg"
+  },
+  {
+    "revision": "348c23799511e7fb657ffd22972b48de",
+    "url": "/moscleanservice/static/media/check.348c2379.svg"
+  },
+  {
+    "revision": "6486785fcfde8f1b884fac60cea430d9",
+    "url": "/moscleanservice/static/media/closeMenu.6486785f.svg"
+  },
+  {
+    "revision": "31de6887912c04a26dc9e73753b1c536",
+    "url": "/moscleanservice/static/media/dishes.31de6887.svg"
+  },
+  {
+    "revision": "b4c1e74f5de6305fce9da7e0a55677fd",
+    "url": "/moscleanservice/static/media/f1.b4c1e74f.jpg"
+  },
+  {
+    "revision": "6711acf1507988fcd6fe998c567411e9",
+    "url": "/moscleanservice/static/media/f2.6711acf1.jpg"
+  },
+  {
+    "revision": "12c69d333e82ff024a645ea49389c761",
+    "url": "/moscleanservice/static/media/f3.12c69d33.jpg"
+  },
+  {
+    "revision": "6f5464bbf2652a24fc8d36e977a102b1",
+    "url": "/moscleanservice/static/media/garbage.6f5464bb.svg"
+  },
+  {
+    "revision": "2bec412cc881ca5d93b8aaea2f7d743b",
+    "url": "/moscleanservice/static/media/image3.2bec412c.jpg"
+  },
+  {
+    "revision": "7c6616ad2688c3bbc7d5ce16dc703070",
+    "url": "/moscleanservice/static/media/logoGreen.7c6616ad.svg"
+  },
+  {
+    "revision": "88a8f5595b5742be740ddbc7c12ff0bb",
+    "url": "/moscleanservice/static/media/m1.88a8f559.jpg"
+  },
+  {
+    "revision": "4f3bf372310a8e42707c2255408936f0",
+    "url": "/moscleanservice/static/media/m2.4f3bf372.jpg"
+  },
+  {
+    "revision": "ceefafb04d010c022461ae224e94afc8",
+    "url": "/moscleanservice/static/media/matress-child.ceefafb0.svg"
+  },
+  {
+    "revision": "85fc81e9359f16435dc55a75c0d4cd6e",
+    "url": "/moscleanservice/static/media/matress-small.85fc81e9.svg"
+  },
+  {
+    "revision": "06eff49a8f1c6db581a5df6e7999254a",
+    "url": "/moscleanservice/static/media/mattress-big.06eff49a.svg"
+  },
+  {
+    "revision": "f2627f563a6ddd5bee65b569ca872856",
+    "url": "/moscleanservice/static/media/s1bg1.f2627f56.jpg"
+  },
+  {
+    "revision": "801c97d012f41aa16fbf6c0d4ec54779",
+    "url": "/moscleanservice/static/media/s1bg2.801c97d0.svg"
+  },
+  {
+    "revision": "bba972bb88305a49e8b3b3064337cb45",
+    "url": "/moscleanservice/static/media/s1bg2Cleaning.bba972bb.svg"
+  },
+  {
+    "revision": "a407001ba77c079c0d1ddf54d9e0001b",
+    "url": "/moscleanservice/static/media/s1bg2washing.a407001b.svg"
+  },
+  {
+    "revision": "07b295d20b13559f54ad28aeb88d3a2a",
+    "url": "/moscleanservice/static/media/s2carpet.07b295d2.jpg"
+  },
+  {
+    "revision": "159e566384005a24822a4f2206452cee",
+    "url": "/moscleanservice/static/media/search.159e5663.svg"
+  },
+  {
+    "revision": "92a906a1baeb884d6e3a9b4914f55f75",
+    "url": "/moscleanservice/static/media/sectionThreeVacuum.92a906a1.jpg"
+  },
+  {
+    "revision": "e576c0383da544d5b1f9870e5ef98088",
+    "url": "/moscleanservice/static/media/sofa2.e576c038.svg"
+  },
+  {
+    "revision": "58a890761fa84acc4b4fd8c729824e69",
+    "url": "/moscleanservice/static/media/stool.58a89076.svg"
+  },
+  {
+    "revision": "c0fd403e9127e13d95af5a54c19161fd",
+    "url": "/moscleanservice/static/media/vacuum.c0fd403e.svg"
+  },
+  {
+    "revision": "033c452420063d1f3950907d29474c5c",
+    "url": "/moscleanservice/static/media/w1.033c4524.jpg"
+  },
+  {
+    "revision": "b39baec8f3958997cf80f17f5a6f2f48",
+    "url": "/moscleanservice/static/media/w2.b39baec8.jpg"
+  },
+  {
+    "revision": "3bf7b3b11fd0fbd2aeb199026c31e58f",
+    "url": "/moscleanservice/static/media/window1.3bf7b3b1.svg"
+  },
+  {
+    "revision": "c95793a635f1bbbfd0140bc465cca9c0",
+    "url": "/moscleanservice/static/media/window2.c95793a6.svg"
+  }
+]);
